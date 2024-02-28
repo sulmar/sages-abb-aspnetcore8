bf20991f6a17ffa67ef23563f241e5d9143cbeef
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-02-28 16:10:15 +0100

    Utworzenie customers endpoints

---
+++ src/Sakila.Api/Endpoints/CustomersEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using Sakila.Domain.Abstractions;
+
+namespace Sakila.Api.Endpoints;
+public static class CustomersEndpoints
+{
+    public static RouteGroupBuilder MapCustomersApi(this RouteGroupBuilder group)
+    {
+        // GET api/customers
+        group.MapGet("/", async (ICustomerRepository repository) => await repository.GetAllAsync());
+
+        // GET api/customers/{id}
+        group.MapGet("/{id}", async (int id, ICustomerRepository repository) =>
+        {
+            var customer = await repository.GetAsync(id);
+
+            if (customer is null)
+            {
+                return Results.NotFound();
+            }
+
+            return Results.Ok(customer);
+        });
+
+        return group;
+    }
+}
