e5c6a29ee837854f4c8b5f2675766cc98ab3dccf
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-02-29 13:50:12 +0100

    Utworzenie klienta usługi

---
+++ src/Sakila.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sakila.ConsoleApp;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var uri = "http://localhost:5220/";
+        swaggerClient client = new swaggerClient(uri, new HttpClient());
+
+         var customers = await client.CustomersAllAsync();
+    
+        foreach (var customer in customers)
+        {
+            Console.WriteLine(customer.FirstName);
+        }
+    }
+}
