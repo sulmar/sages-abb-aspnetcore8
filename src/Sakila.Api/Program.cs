73fe9650cc3ae0059807d808068f5ccdbea5f564
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-02-29 13:49:49 +0100

    Dodanie OpenApi

--- src/Sakila.Api/Program.cs
+++ src/Sakila.Api/Program.cs
@@ -1,14 +1,11 @@
-using Microsoft.AspNetCore.Components;
 using Json = Microsoft.AspNetCore.Http.Json;
-using Microsoft.EntityFrameworkCore;
 using Sakila.Domain.Abstractions;
-using Sakila.Infrastructure;
-using Sakila.Intrastructure;
 using System.Text.Json.Serialization;
 using Sakila.Api.Endpoints;
 using Sakila.Api.Extensions;
 using Sakila.Domain.SearchCritierias;
 using Microsoft.AspNetCore.Mvc;
+using Sakila.Domain.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +17,15 @@ builder.Services.Configure<Json.JsonOptions>(options =>
     options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 });
 
+
+// dotnet add package Swashbuckle.AspNetCore
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", ([FromHeader(Name = "x-code")] string? code) => $"Hello {code}!!!");
  
@@ -32,14 +37,31 @@ app.MapGroup("api/customers")
 app.MapGroup("api/payments")
     .MapPaymentsApi();
 
-// GET api/films?title=Lorem&rating=PG
-app.MapGet("api/films", ([AsParameters] FilmSearchCritieria critieria, IFilmRepository repository) => repository.GetByAsync(critieria));
 
+// dotnet add package Microsoft.AspNetCore.OpenApi
+// GET api/films?title=Lorem&rating=PG
+app.MapGet("api/films", ([AsParameters] FilmSearchCritieria critieria, IFilmRepository repository) => repository.GetByAsync(critieria))
+      .WithOpenApi(operation => new(operation)
+      {
+          Summary = "This is a summary",
+          Description = "This is a description"
+      });
+
+app.MapPost("api/photos", (IFormFile file) =>
+{
+    return Results.Ok();
+}).DisableAntiforgery();
 
 app.MapGet("/Hello", async (HttpRequest req, HttpResponse res) =>
 {
     await res.WriteAsync("Hello World!");
     res.StatusCode = 200;
+});
+
+
+app.MapPost("api/actors", async ([FromForm] Actor actor, IActorRepository repository) =>
+{
+    await repository.AddAsync(actor);
 });
 
 app.Run();
